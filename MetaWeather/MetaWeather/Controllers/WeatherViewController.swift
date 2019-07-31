e63ecb2fb740b7ef7891f70ae2a81396d0b4c36e
tanakaderoy <42681052+tanakaderoy@users.noreply.github.com>
2019-07-31 13:45:03 -0500

    can ask for location

---
+++ MetaWeather/MetaWeather/Controllers/WeatherViewController.swift
@@ -0,0 +1,70 @@
+//
+//  WeatherViewController.swift
+//  MetaWeather
+//
+//  Created by Tanaka Mazivanhanga on 7/31/19.
+//  Copyright © 2019 Tanaka Mazivanhanga. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var currentCityLabel: UILabel!
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0{
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            print("longitude \(location.coordinate.longitude) latitude \(location.coordinate.latitude)")
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        currentCityLabel.text = "Location Unavailable"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension WeatherViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherTableViewCell
+        cell.maxTemperatureLabel.text = "32"
+        return cell
+        
+    }
+    
+    
+}
---
+++ MetaWeather/MetaWeather/Models/Networking.swift
@@ -0,0 +1,11 @@
+//
+//  Networking.swift
+//  MetaWeather
+//
+//  Created by Tanaka Mazivanhanga on 7/31/19.
+//  Copyright © 2019 Tanaka Mazivanhanga. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
