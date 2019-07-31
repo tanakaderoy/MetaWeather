13f8089092cfc6a0642172823151f90e057555ca
tanakaderoy <42681052+tanakaderoy@users.noreply.github.com>
2019-07-31 13:20:20 -0500

    got the first screen set up how i want. next connect to api and make data models for it

---
+++ MetaWeather/MetaWeather/Views/WeatherTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherTableViewCell.swift
+//  MetaWeather
+//
+//  Created by Tanaka Mazivanhanga on 7/31/19.
+//  Copyright © 2019 Tanaka Mazivanhanga. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var minTemperatureLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var maxTemperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var percentChanceLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
